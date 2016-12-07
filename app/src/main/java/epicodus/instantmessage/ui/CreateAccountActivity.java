cb609efde2c6e44164bd69e278e71c6974ab3693
Caleb Paul and Joanna Anderson <caleb+joanna@>
2016-12-07 09:55:19 -0800

    integrate firebase auth, add login and signup activities

---
+++ app/src/main/java/epicodus/instantmessage/ui/CreateAccountActivity.java
@@ -0,0 +1,15 @@
+package epicodus.instantmessage.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import epicodus.instantmessage.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
